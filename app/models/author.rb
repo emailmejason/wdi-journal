701207b310f9f2605702e410bc4abd28941378df
Sumeet Jain <sumeet@sumeetjain.com>
2013-06-12 15:08:22 -0700

    User Auth: This sets up the Author class to have a secure password

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,7 @@
 class Author < ActiveRecord::Base
-  attr_accessible :age, :bio, :first_name, :last_name
+  has_secure_password
+
+  attr_accessible :age, :bio, :first_name, :last_name, :email, :password, :password_confirmation
+
+  validates :email, :uniqueness => true
 end
