e2b89cf1a389ab85d37cf0c7ed1ec3a1747189a4
Sumeet Jain <sumeet@sumeetjain.com>
2013-06-13 14:26:56 -0700

    Add a note about helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,12 @@ class ApplicationController < ActionController::Base
       @current_author = Author.find(session[:author_id])
     end
   end
+
+  # Remember: ApplicationController sets methods available to all
+  # other controllers. But these methods aren't available in any
+  # of the views. View methods are called "helpers" (See the app/helpers
+  # folder). The `helper_method` line below adds the 'current_author'
+  # controller method as a helper method as well. So now we can use
+  # `current_author` in views.
   helper_method :current_author
 end
